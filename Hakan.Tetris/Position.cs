1b47c0df1bcdf68cc9e9007bc61dd07abf9edc3f
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2022-09-19 19:19:11 +0300

    Add files via upload

---
+++ Hakan.Tetris/Position.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Hakan.Tetris
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
---
+++ Hakan.Tetris/TBlock.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Hakan.Tetris
+{
+    public class TBlock:Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] {new(0,1), new(1,0), new(1,1), new(1,2)},
+            new Position[] {new(0,1), new(1,1), new(1,2), new(2,1)},
+            new Position[] {new(1,0), new(1,1), new(1,2), new(2,1)},
+            new Position[] {new(0,1), new(1,0), new(1,1), new(2,1)}
+        };
+
+        public override int ID => 6;
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
